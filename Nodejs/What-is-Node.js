a292c3d672495010b0c4f49f8724709e22802eff
Riajul Islam <riajulpislam@gmail.com>
2024-01-23 23:35:37 +0600

    why-node

--- Nodejs/What-is-Node.js
+++ Nodejs/What-is-Node.js
@@ -1,8 +1,27 @@
 //What is Node js (Introduction)
 
 //Is Node js a programming language?
-//Node.js kono programming language na. Nodejs holo akta platform. je platform use kore amra javascript ke server-side a run korte pari.and seei platform er akta nejosso library ase, jeei library bivinno doroner programming interface provide kore seei server-side operation golo ke manage korar jonno.Nodejs hocche programming er jonno akta run tine macchine.
+//Node.js kono programming language na. Nodejs holo akta platform. je platform use kore amra javascript ke server-side a run korte pari.and seei platform er akta nejosso library ase, jeei library bivinno doroner programming interface provide kore seei server-side operation golo ke manage korar jonno.Nodejs hocche programming er jonno akta run time macchine.
 
 
 //Jeei feature golo sorasori browser er opor depandent/nirborshil ,seei feature golo Nodejs a kaj korbe na.document.write nodejs a kaj korbe na.tokhon console.log lekhte hobe.
 //window/obj/Navigatorobj/event/dom ae feature golo Nodejs a run hobe na.manee aegolor kono defination nodejs a nae. oigolo Browser er opor nirvishil/depandent.
+
+
+//* What is Node.js Server?
+//Node.js server akta req-res model complete korar akta system.
+
+//*Why Node.js Server? node.js er kaj ta ki/node.js keno dorkar?
+//(Database Communication) Database er sathe jodi Communication korte chai, Node.js Server er use/throw teei korte hobe. Another frontend theke Database er sathe  Communication korte parbo na.ar Dtabase er sathe jodi Communication na hoy, tahole Software banate parbo na. Karon Software er bitore amra Dynamacly ja kiso dekhi,tar sobkisoe Database thekeei ashe.
+//(1)->Execution Security
+//Joto Security Execution feature sobgolo neye jabo Backend a. Backend Secure hole Software Secure hobe.
+//2->B2B
+//Business to Business Communication. Exm-> SMS/Email/Paymant Getway
+//3->Expensive Execution
+//4->Managing File Directory
+
+
+//* Important
+//Node.js/Express.js/Mongodb use kore Backend development korsi.tar mane sojakotha ami (RestApi)/Server-side develop korsi.
+//Seei server-side ta Test korar jonno/ documentation create korte hobe, seei jonno (Postman sofwtare) use korbo.
+//Agee amra Javascript er code Node.js a run korsi. Akhon amra Backend er jonno jeei code golo lekhbo, seei code golo Node.js Server a run hobe. tarjonno amar first a Server Create korbo,tarpor code run korbo.
