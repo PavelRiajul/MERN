2bcc9966fb2248b56cda177fc3a02be008e6ecff
Riajul Islam <riajulpislam@gmail.com>
2024-01-31 12:42:14 +0600

    web

--- Express-js/What-is-Express-js.js
+++ Express-js/What-is-Express-js.js
@@ -2,6 +2,23 @@
 //This is a server web framework for node.js.
 //Express.js Node.js er akta framework. jeei framework deye amader Software/Application seitar jeei server-side ase, seei server-side er part ke manage kora.
 
+//Express.js amader Application er Backend manage kore.
+//Express.js deye amra moloto Rest-Api Develope korbo.
+
+//Rest Api/Backend project a kono view thake na.--> m-model, c-controller = this is MVC but view ta thake na backend project a .
+
+//Jokhon amra Frontend neye kaj korbo, tokhon Application er bitore view ashbe.Oi view ta mern-stack a React er view hobe.= this is MVC pattern.
+
+//Exxpress.js Application er bitore completely sodu Backend ase. sodo Backend project a kono View thake na.
+
+//Jokhon amra Backend  a kaj korbo,tokhon amar Frontend a Mobile Application thakbe  na ki Web Application thakbe oita amar chintar bisoe na. amar chinter bisoe hocche ami Backend er Features thikthak vabe banate parsi ki na.
+
+//*Example-
+//ami akta E-commerce er Backend develope korchi ,ami Backend developer. amar kaj hocche oi E-commerce er Backend develope kora.akhon ae E-commerce er Frontend ta akta Mobile-Application hote pare ba Decstop Application hote pare.amar kaj hocce backend feature golo thikthak vabe banate parsi kina.
+
+//Akhon kinto sob Microservices architecture a Software Indersty chole.tar manee jokhon akta Backend develope kora hoy, backend ta ke amon vabe develope kora hoy jeta sob Frontend a chole.
+
+//Backend developer der kaj hocche Backend er documentation ta Frontend developer ke deye deya.akhon se oita neye mobile/app developer korbe na ki web/app develope korbe, seeita tar bepar.
 
 //What is Postman ?
 //Postman hocche akta software, jeei software deye Reat-Api ke test kora jai.
