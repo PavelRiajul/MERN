986159f988b201df8fcf9f9415f1acba10749c14
Riajul Islam <riajulpislam@gmail.com>
2024-02-04 14:52:34 +0600

    jwt

--- Web-Terminology/Encryption-Dectiption.js
+++ Web-Terminology/Encryption-Dectiption.js
@@ -8,7 +8,7 @@
 //sending and reciving ae 2 ta event er maj khane moloto hoy Encryption Decryption er gotona ta gote.
 
 //sender er kase (plaintext) thake.jeei plaintext ta hocche humanredable ba je kew dekhe seta porte pare.
-//sender public key use kore   Encryption process er maddome seta ke Encrypt kore  setale plaintext theke Ciphertext a porinito kore.
+//sender public key use kore   Encryption process er maddome seta ke Encrypt kore  setake plaintext theke Ciphertext a porinito kore.
 // acually sender reciver er kase ciphertext tae patheye deei.orthat sender er kaj theke jokhon kono data send hocche , seei data ta public key er maddome  Encryption process a  seei data ta plaintext theke Encrypt obostai ciphertext a porinoto hocche.
 
 //akhon ae ciphertext humanredable noe,aeta kew dekhle bojte parbe na  sender sei ciphertext ta ke reciver er kase patheye decce.akhon reciver er kase jokhon sei ciphertext ta ashtese reciver a kaseo akta (Private key ase) reciver seei privet key deye oi je se ciphertext ta recive korlo sei ciphertext ta ke Decryption process er maddome se abar ponorai humanredable plaintext a poriniti kortese.
---
+++ Web-Terminology/JWT/JWT-concept.js
@@ -0,0 +1,18 @@
+// JSON WEB TOKE(JWT)
+
+// what is JWT? JWT er kaj ta ki ?
+// amra jodi akadin sender reciver er modde  khob securely data  transmit korte chai , seta jodi Json object er maddome korte chai ba Json a korte chai , seei khetre  Json Web Token hocche akta way.
+// Jokhon amra JWT er maddome data Encrypt decrypt kore patacchi , Encrypt decrypt korar somoy amra  public key use kortesi for ta Encrypt perpases and private key use kortesi for the Decrypt perpascess, jeheto aekhane data digitally  private key public key use kore  sign kora hochi ba akebare rap kora hocesee, so aeta verified and truted akta maddom.
+
+
+// JSON jeei data, ae data er opore jokhon amra kono Encryption Decryption process chalacchi , tokhon seei process ta amader  Ecosystem ba amader indersty te  JSON WEB TOKEN(JWT) name beshi porichito.
+
+//*JWT USES
+// Authorization:  jwt er akta  bohol bebohar hocche amader  user er Autherization,Authentication,routes,services and resources er opore bivinno rectrction bosano. aegolo amra JWt deye use kore thakee.
+//asara o er main jeei karon , seei karon ta hocessi json ke Encrypt Decrypt kora . akhon ae json ke Encrypt Decrypt kora maddome  joto doroner kaj kora possible sob doroner kaj  amra jwt deye kore thakee.
+//Information Exchange:
+//......
+//Jokhon amra JWT neye kaj korbo tokhon kivabe jwt ke Encrypt kori Decrypt kori  ae bisoe golo amader ke bojte hobe.
+
+//*website
+//jwt.io -> jwt holo akta library je library ta bivinno platform er jonno avileable .
